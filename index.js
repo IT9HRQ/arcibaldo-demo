750f2d152ebfc2df86863c4416fcdf529892cb42
francesco <info.francescobianco@gmail.com>
2019-03-25 15:11:11 +0100

    fix style css deps

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './pages'));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
     res.render('index');
